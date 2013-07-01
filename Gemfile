067f1120e42eab8e1951ad4ad2050b66e8eccf1b
Praveena G <praveena.gunasekhar@gmail.com>
2013-07-01 23:26:25 +0530

    Praveena | Added rails to gemfile and sample app

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source "https://rubygems.org"
 
+gem 'rails', '3.2.13'
 gem 'bunny'
 gem 'ohai'
 gem 'knife-solo' #, :git => "https://github.com/matschaffer/knife-solo.git"
---
+++ sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'c995f4e5662e9c4133be10b8575259dbd644bc62363bd4e58f41a1a41c1c2cab911dc22de6641f5ace12ee13829886d2251b5d01baca22768f81e65495dbcb11'
